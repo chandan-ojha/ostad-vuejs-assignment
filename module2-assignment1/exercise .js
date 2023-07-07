9114509d5bdaab1beaf53717432ed604c9d0a034
Chandan Ojha <chandanojha776@gmail.com>
2023-07-07 22:57:04 +0600

    `exercise-4` solved

--- module2-assignment1/exercise .js
+++ module2-assignment1/exercise .js
@@ -24,3 +24,10 @@ function createGreeting(name) {
 }
 
 console.log(createGreeting("Alice"));
+
+//Exercise 4: Write a function named isEven that takes in a number as a parameter and returns the string "Even" if the number is even, and "Odd" if the number is odd. Use a ternary operator instead of an if/else statement.
+function isEven(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
+
+console.log(isEven(7));
