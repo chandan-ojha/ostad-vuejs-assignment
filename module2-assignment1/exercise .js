c33fc9576a34a037853e62a1c39f10586ec95a31
Chandan Ojha <chandanojha776@gmail.com>
2023-07-07 20:28:13 +0600

    `exercise-1` solved

--- module2-assignment1/exercise .js
+++ module2-assignment1/exercise .js
@@ -1,1 +1,12 @@
 //Exercise 1: Write a function named destructureExample that takes in an object and an array as parameters. The function should use destructuring to extract the values 'name' and 'age' from the object and the values at index 0 and index 2 from the array. The function should then return an object with the extracted values as properties with name and age.
+const obj = { name: "John", age: 30, city: "New York" };
+const arr = [10, 20, 30, 40];
+
+function destructureExample(obj, arr) {
+  const { name, age } = obj;
+  const [index0, index1] = arr;
+  return { name, age, index0, index1 };
+}
+
+const result = destructureExample(obj, arr);
+console.log(result);
