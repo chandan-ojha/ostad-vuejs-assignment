62cf4182e21dc0682d77f594f7ef804799a04af2
Chandan Ojha <chandanojha776@gmail.com>
2023-07-07 19:17:34 +0600

    start-assignment

---
+++ module2-assignment1/script.js
@@ -0,0 +1,1 @@
+//Exercise 1: Write a function named destructureExample that takes in an object and an array as parameters. The function should use destructuring to extract the values 'name' and 'age' from the object and the values at index 0 and index 2 from the array. The function should then return an object with the extracted values as properties with name and age.
