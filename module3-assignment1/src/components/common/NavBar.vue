e74b6969ff8adb3a5d3dc4d2a80ba32a855afb25
Chandan Ojha <chandanojha776@gmail.com>
2023-07-17 19:36:57 +0600

    style binding added

--- module3-assignment1/src/components/common/NavBar.vue
+++ module3-assignment1/src/components/common/NavBar.vue
@@ -1,4 +1,7 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+let activeMenu = ref(null);
+</script>
 
 <template>
   <nav class="navbar main-nav navbar-expand-lg px-2 px-sm-0 py-2 py-lg-0">
@@ -19,28 +22,46 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="/" data-toggle="dropdown"
+          <li class="nav-item dropdown" @click="activeMenu = 'home'">
+            <a
+              class="nav-link dropdown-toggle"
+              href="/"
+              data-toggle="dropdown"
+              :class="{ active: activeMenu === 'home' }"
               >Home
             </a>
           </li>
-          <li class="nav-item dropdown @@pages">
-            <a class="nav-link dropdown-toggle" href="/" data-toggle="dropdown"
+          <li class="nav-item dropdown @@pages" @click="activeMenu = 'pages'">
+            <a
+              class="nav-link dropdown-toggle"
+              href="/"
+              data-toggle="dropdown"
+              :class="{ active: activeMenu === 'pages' }"
               >Pages
               <span><i class="ti-angle-down"></i></span>
             </a>
             <!-- Dropdown list -->
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item @@team" href="/">Team</a></li>
-              <li><a class="dropdown-item @@career" href="/">Career</a></li>
-              <li><a class="dropdown-item @@blog" href="/">Blog</a></li>
+              <li><a class="dropdown-item @@team" href="#">Team</a></li>
+              <li><a class="dropdown-item @@career" href="#">Career</a></li>
+              <li><a class="dropdown-item @@blog" href="#">Blog</a></li>
             </ul>
           </li>
-          <li class="nav-item @@about">
-            <a class="nav-link" href="/">About</a>
+          <li class="nav-item @@about" @click="activeMenu = 'about'">
+            <a
+              class="nav-link"
+              href="#"
+              :class="{ active: activeMenu === 'about' }"
+              >About</a
+            >
           </li>
-          <li class="nav-item @@contact">
-            <a class="nav-link" href="/">Contact</a>
+          <li class="nav-item @@contact" @click="activeMenu = 'contact'">
+            <a
+              class="nav-link"
+              href="#"
+              :class="{ active: activeMenu === 'contact' }"
+              >Contact</a
+            >
           </li>
         </ul>
       </div>
@@ -48,4 +69,8 @@
   </nav>
 </template>
 
-<style scoped></style>
+<style>
+.active {
+  color: #2e7eed !important;
+}
+</style>
