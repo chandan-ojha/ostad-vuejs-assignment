b4d33004ef5099d76d729d8651925132ed03ba04
Chandan Ojha <chandanojha776@gmail.com>
2023-07-23 20:25:23 +0600

    `image carousel` done

---
+++ module4-assignment1-image-carousel/src/data/data.js
@@ -0,0 +1,34 @@
+const carouselItems = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1682687980918-3c2149a8f110?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    title: "First Slide Label",
+    caption: "caption 1",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1682687982185-531d09ec56fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    title: "Second Slide Label",
+    caption: "caption 2",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1682695797873-aa4cb6edd613?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    title: "Third Slide Label",
+    caption: "caption 3",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1682687982046-e5e46906bc6e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    title: "Fourth Slide Label",
+    caption: "caption 4",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1682687982167-d7fb3ed8541d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80",
+    title: "Fifth Slide Label",
+    caption: "caption 5",
+  },
+];
+
+export { carouselItems };
