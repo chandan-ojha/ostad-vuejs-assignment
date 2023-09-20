89c299f4ede1ba00613fba083cc3da3c2a6ad762
Chandan Ojha <chandanojha776@gmail.com>
2023-09-20 22:52:43 +0600

    updated

--- simple-shop-website/src/components/common/Footer.vue
+++ simple-shop-website/src/components/common/Footer.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 <template>
   <footer class="bg-gray-200 text-center py-4 mt-5">
-    <p>&copy; 2023 Shop Website Developed By <strong>Chandan</strong></p>
+    <p>&copy;2023 Shop Website Developed By <strong>Chandan</strong></p>
   </footer>
 </template>
 
