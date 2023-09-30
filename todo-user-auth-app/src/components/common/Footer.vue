f5544f378acb1a9e68dbd53a8d69a50f8bce9e2b
Chandan Ojha <chandanojha776@gmail.com>
2023-09-30 09:57:47 +0600

    `icon-added`

--- todo-user-auth-app/src/components/common/Footer.vue
+++ todo-user-auth-app/src/components/common/Footer.vue
@@ -2,7 +2,14 @@
   <!-- Footer -->
   <footer class="bg-blue-500 text-white p-2 fixed bottom-0 left-0 w-full">
     <div class="container mx-auto text-center">
-      &copy;2023 Pinia. All Rights Reserved by <strong>Chandan</strong>
+      &copy;2023 Pinia
+      <img
+        src="https://img.icons8.com/emoji/48/000000/pineapple-emoji.png"
+        alt="pineapple"
+        width="20"
+        height="20"
+        class="inline-block align-text-top"
+      />. All Rights Reserved by <strong>Chandan</strong>
     </div>
   </footer>
 </template>
--- todo-user-auth-app/src/components/common/NavBar.vue
+++ todo-user-auth-app/src/components/common/NavBar.vue
@@ -5,7 +5,16 @@ import { RouterLink } from "vue-router";
   <!-- Navigation Bar -->
   <nav class="bg-blue-500 text-white p-3 sticky top-0">
     <div class="container mx-auto flex justify-between items-center">
-      <a href="/" class="text-2xl font-semibold">Pinia</a>
+      <a href="/" class="text-2xl font-semibold">
+        Pinia
+        <img
+          src="https://img.icons8.com/emoji/48/000000/pineapple-emoji.png"
+          alt="pineapple"
+          width="30"
+          height="30"
+          class="inline-block align-text-top"
+        />
+      </a>
       <ul class="flex space-x-4">
         <li>
           <RouterLink to="/" class="hover:text-gray-300">Home</RouterLink>
